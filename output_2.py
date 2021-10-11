821be2a8d2ad7f86624715eb7a73e3e779c3c187
Krishna Dodda <80235773+KrishnaSindhuReddyDodda@users.noreply.github.com>
2021-10-11 17:20:28 -0500

    Add files via upload

---
+++ output_2.py
@@ -0,0 +1,9 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:\Users\dksin\Dropbox\My PC (LAPTOP-E383CH2K)\Desktop\2.py =====
+The individual signatures of C1, C2, C3 are......
+Signature for C1 [1, 2, 3]
+Signature for C2 [1, 2, 3]
+Signature for C3 [14, 15, 16]
+>>> 
